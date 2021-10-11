eca9107f1a133705f71ddfef7c3c58a72a50351b
Tanu Grover <v-grovertanu@microsoft.com>
2021-10-12 02:25:37 +0530

    Tesla-Clone commit

---
+++ src/components/Home.js
@@ -0,0 +1,63 @@
+import styled from "styled-components"
+import Section from "./Section"
+
+const Home = () => {
+    return (
+        <Container>
+            <Section 
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back Gaurantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section 
+                title="Solar for New Roofs"
+                description="Solar Roof Costs Less Than a New Roof Plus Solar panels"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section 
+                title="Accessories"
+                description="Long Life Genuine Accessories"
+                backgroundImg="accessories.jpg"
+                leftBtnText="Order Now"
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
